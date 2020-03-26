80ad943ba41ffa6e454fe9ad8e151a77ef3fa299
mai cook <mailiecook@yahoo.com>
2020-03-26 13:52:47 -0700

    added end selector tag line 19, debug scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $("button").click(function(event) {
     event.preventDefault();
+      var selector =$(#selector).val();
+      if(selector === "cheetah") {
+        $(.cheetah).show();
+      }
 
-    
+
 //     var answer = $("input:checked").val();
 //       return answer.show(function();
 //     });
